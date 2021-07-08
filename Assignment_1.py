bd8c0719073a23bdf025a44bea1bb67ed1602035
Anushka000 <85939690+Anushka000@users.noreply.github.com>
2021-07-08 18:05:03 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,5 @@
+x=float(input("Input the radius of the cirle:"))
+a=3.14*x*x
+print("The area of the circle with radius is",a)
+
+
---
+++ Assignment_2.py
@@ -0,0 +1,3 @@
+x=str(input("Input the filename:"))
+y=x.split(".")
+print("The extension of the file is:",(y[-1]))
