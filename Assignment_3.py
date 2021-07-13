70feaa96649c16c922ceeb8fea2d335b36536e48
Anushka000 <85939690+Anushka000@users.noreply.github.com>
2021-07-13 18:54:46 +0530

    Add files via upload

---
+++ Assignment_3.py
@@ -0,0 +1,21 @@
+list1=[12,-7,5,64,-14]
+print("input list is:",list1)
+l=[]
+
+for i in list1:
+    if i>0:
+        l.append(i)
+    else:
+        continue
+
+x,y,z=l
+print("ouput is:",x,y,z)
+
+
+
+
+
+
+    
+    
+        
---
+++ Assignment_4.py
@@ -0,0 +1,12 @@
+list1=[12,14,-95,3]
+print("input list is:",list1)
+l=[]
+
+for i in list1:
+    if i>0:
+        l.append(i)
+    else:
+        continue
+
+print("ouput:",l)
+    
